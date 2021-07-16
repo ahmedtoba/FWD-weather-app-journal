b475156763c73a04541aa328e1c41bc8b1bedbfc
Ahmed Toba <engahmedtoba@yahoo.com>
2021-07-16 15:56:33 +0200

    extracting data from the EXternal API

--- server.js
+++ server.js
@@ -2,18 +2,36 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server
+// Setting the Server up on port 8000
+const port = 8000;
+app.listen(port, ()=>{
+  console.log(`Server is running on: http://localhost:${port}`);
+})
+
+// get route to send the Internal API data to the client side
+app.get('/sendData', (req, res)=>{
+  res.send(projectData)
+})
+
+//post route to receive the data obtaidned from the external API from the client side and push it to the projectData object
+
+
+
+
+// post route to recieve the data returned from the weather API
--- website/app.js
+++ website/app.js
@@ -3,3 +3,44 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// the basic url without ziocode or apiKey
+basicURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+// defining the api key
+const apiKey = 'dfbe2f1a6d3fb2a41435b4828f580224';
+// selecting generate button with the id #generate to apply the event listener on it
+const genButton = document.getElementById('generate');
+
+// applying event listener when clicking generate button
+genButton.addEventListener('click', obtainData)
+
+// a function to obtain the data from the weather api
+function obtainData() {
+    // getting the value of the zipCode when entered by the user in the textarea
+    const zipCode = document.getElementById('zip').value;
+    // alerting the user when no zipcode entered
+    if (zipCode==='') {
+      alert('Enter Zipcode first!');
+      // adding retrun to get out the function and don't the complete the following steps
+      return
+    }
+    // fetching the data from the external api using the zipcode and the apikey
+    fetchData(basicURL, zipCode, apiKey);
+}
+
+// async fetchData function that uses fetch to get the data from the external api
+const fetchData = async (url, zip, key) => {
+  // using await to wait until there is a response from the external api
+  const responseData = await fetch(url + zip + '&appid=' + key + '&units=metric');
+  // handling the data obtained using try to transform the json data extracted from the API to javascript object
+  try {
+    const obtainedData = await responseData.json();
+    console.log(obtainedData);
+    return obtainedData.main.temp
+  }
+  // and using catch to alert the user that the zipcode is wrong and displaying the error in the console
+  catch (err) {
+      console.log("ERRRRRor", err)
+      alert('Zip Code Entered is not valid!')
+  }
+}
